a577bfaccb9a5c238028c036f64d0789e88ced64
gxz <cnmmail163@163.com>
2023-11-04 00:46:03 +0800

    安装MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import {createTheme, CssBaseline, ThemeProvider, useMediaQuery} from '@mui/material'
+import {useMemo} from 'react'
+import Site from './Site.tsx'
+
 export default function App() {
-  return null
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  const theme = useMemo(() => createTheme({palette: {mode: prefersDarkMode ? 'dark' : 'light'}}), [prefersDarkMode])
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Site/>
+    </ThemeProvider>
+  )
 }
