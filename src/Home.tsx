50232122e15463663b4768883d2554ec3b364971
gxz <cnmmail163@163.com>
2023-11-04 14:34:10 +0800

    完成首页

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,18 +1,14 @@
-import {Box, Typography} from '@mui/material'
+import {Box, Button, ButtonGroup, Typography} from '@mui/material'
 
 export default function Home() {
   return (
-    <Box my={2}>
-      <Typography variant="h1" id="1">你好，世界！</Typography>
-      <Typography variant="h1" id="2">你好，世界！</Typography>
-      <Typography variant="h1" id="3">你好，世界！</Typography>
-      <Typography variant="h1" id="4">你好，世界！</Typography>
-      <Typography variant="h1" id="5">你好，世界！</Typography>
-      <Typography variant="h1" id="6">你好，世界！</Typography>
-      <Typography variant="h1" id="7">你好，世界！</Typography>
-      <Typography variant="h1" id="8">你好，世界！</Typography>
-      <Typography variant="h1" id="9">你好，世界！</Typography>
-      <a href="#1">1</a>
+    <Box my={20} textAlign="center">
+      <Typography variant="h1" paragraph>模板网站</Typography>
+      <Typography variant="subtitle1" paragraph>Vite + React + TS + SWC + MUI + React Router</Typography>
+      <ButtonGroup size="large">
+        <Button variant="contained" href="打卡">立即上手</Button>
+        <Button href="https://github.com/iamnottsh/vite-mui-template" target="_blank" rel="noreferrer">Github</Button>
+      </ButtonGroup>
     </Box>
   )
 }
