63b29b391a8c5a184dadda0e15c153849372f9f4
gxz <cnmmail163@163.com>
2023-11-08 11:29:17 +0800

    重命名

--- src/路由/讲义/children/达成目标/children/维持情谊/children/index.ts
+++ src/路由/讲义/children/达成目标/children/维持情谊/children/index.ts
@@ -2,9 +2,9 @@ import {TreeProps} from '../../../../../../Tree.tsx'
 import 不袭击 from './不袭击'
 import 不裹挟 from './不裹挟'
 import 不评论 from './不评论'
-import 不践踏 from './不践踏'
+import 不鄙夷 from './不鄙夷'
 import 不冷淡 from './不冷淡'
 import 不解决 from './不解决'
 import 不正经 from './不正经'
 
-export default Object.entries<TreeProps>({不袭击, 不裹挟, 不评论, 不践踏, 不冷淡, 不解决, 不正经})
+export default Object.entries<TreeProps>({不袭击, 不裹挟, 不评论, 不鄙夷, 不冷淡, 不解决, 不正经})
